009c89ea1ac215a12a4d9fa26dd1afa6672c3d6c
harsnyi <harsanyitanulo@gmail.com>
2023-10-25 20:44:57 +0200

    not working

--- src/StudentPlayer.java
+++ src/StudentPlayer.java
@@ -7,23 +7,29 @@ public class StudentPlayer extends Player{
 
     @Override
     public int step(Board board) {
-        return minimax(8,board,false,-10000,10000);
+        return minimax(10,board,true,Integer.MIN_VALUE,Integer.MAX_VALUE);
     }
 
 
     private int minimax(int depth, Board board, boolean maximizingPlayer,int alpha, int beta) {
-        if (depth == 0 || board.gameEnded()) {
-            return evaluatePosition(board);
+
+        if(board.gameEnded()){
+            return maximizingPlayer ? 100 : -100;
+        }
+
+        if (depth == 0) {
+            return calculate(board);
         }
 
         if (maximizingPlayer) {
-            int maxEvaluation = -10000;
+            int maxEvaluation = Integer.MIN_VALUE;
             int bestMove = -1;
-            for (int i = 0; i < 7; i++) {
+            for (int i:board.getValidSteps()) {
                 if (board.stepIsValid(i)) {
-                    Board b = new Board(board);
-                    b.step(playerIndex, i);
-                    int evaluation = minimax(depth - 1, b, false,alpha,beta);
+                    board.step(2, i);
+                    int j = board.getLastPlayerRow();
+                    int evaluation = minimax(depth - 1, board, false,alpha,beta);
+                    board.getState()[j][i] = 0;
                     if (evaluation > maxEvaluation) {
                         maxEvaluation = evaluation;
                         bestMove = i;
@@ -36,13 +42,14 @@ public class StudentPlayer extends Player{
             }
             return bestMove;
         } else {
-            int minEvaluation = 10000;
+            int minEvaluation = Integer.MAX_VALUE;
             int bestMove = -1;
-            for (int i = 0; i < 7; i++) {
+            for (int i:board.getValidSteps()) {
                 if (board.stepIsValid(i)) {
-                    Board b = new Board(board);
-                    b.step(3-playerIndex, i);
-                    int evaluation = minimax(depth - 1, b,true,alpha,beta);
+                    board.step(1, i);
+                    int j = board.getLastPlayerRow();
+                    int evaluation = minimax(depth - 1, board,true,alpha,beta);
+                    board.getState()[j][i] = 0;
                     if (evaluation < minEvaluation) {
                         minEvaluation = evaluation;
                         bestMove = i;
@@ -57,154 +64,110 @@ public class StudentPlayer extends Player{
         }
     }
 
-
-    /**
-     * Utility value = (foursOfTheAI * 10 + threesOfTheAI * 5 + twosOfTheAI * 2) - (opponentFours *10 + opponentThrees * 5 + opponentTwos * 2)
-     * @param board
-     * @return
-     */
-    private int evaluatePosition(Board board){
-
-        int opponentIndex = 3 - playerIndex;
-        int foursOfStudentPlayer = checkForN(4, board,playerIndex);
-        int threesOfStudentPlayer = checkForN(3, board,playerIndex);
-        int twosOfStudentPlayer = checkForN(2, board,playerIndex);
-
-        int foursOfOpponent = checkForN(4, board,opponentIndex);
-        int threesOfOpponent = checkForN(3, board,opponentIndex);
-        int twosOfOpponent = checkForN(2, board,opponentIndex);
-
-        if(foursOfStudentPlayer > 0){
-            return 1500;
+    int isValidPlace(Board b, int row , int col){
+        if(row < 6 && row > -1 && col < 7 && col > -1){
+            return b.getState()[row][col];
         }
-        else if (foursOfOpponent > 0){
-            return -1500;
-        }
-
-
-        return
-                ((threesOfStudentPlayer * 250 + twosOfStudentPlayer * 80)) - ((threesOfOpponent * 250 + twosOfOpponent * 80));
+        else
+            return -1;
     }
 
-    private int checkForN(int N,Board board, int playerIndex){
-        return (checkForNsDiagonally(N, board.getState(),board.getLastPlayerIndex(),playerIndex) +
-                checkForNsInARow(N, board.getState(),board.getLastPlayerIndex(),playerIndex) +
-                checkForNsInAColumn(N, board.getState(),board.getLastPlayerIndex(),playerIndex));
-    }
+    public int calculate(Board board){
+        int row = board.getLastPlayerRow();
+        int col = board.getLastPlayerColumn();
+        int score = 0;
 
-    /**
-     * For checking if there are any possible N-s diagonally.
-     * @param N - number of the same numbers diagonally
-     * @return the number of possible Ns
-     */
-    private int checkForNsDiagonally(int N, int[][] state,int lastPlayer,int playerNumber){
-
-        int connectedNs = 0;
-
-        //Checking from the start of the table
-        for(int i = 0; i<7-N;i++){
-            for(int j = 0;j<=6-N;j++){
-                int count = 0;
-                for(int x = 0;x< N;x++){
-                    if(state[j+x][i+x] == playerNumber){
-                        count++;
-                    }
-                }
-                if(count == N){
-                    connectedNs++;
-                }
-
-            }
+        // UNDER
+        if(isValidPlace(board, row  + 1 , col) == board.getLastPlayerIndex()) {
+            score += 2;
+            if(isValidPlace(board, row  + 2 , col) == board.getLastPlayerIndex())
+                score += 2;
+        }
+        if(isValidPlace(board, row + 1 , col - 1) == board.getLastPlayerIndex()) {
+            score += 2;
+            if(isValidPlace(board, row + 2 , col - 2) == board.getLastPlayerIndex()) {
+                score += 2;
+            }else if(isValidPlace(board, row + 2, col - 2) == 0)
+                score += 1;
+        }else if(isValidPlace(board, row + 1, col - 1) == 0){
+            score += 1;
+            if(isValidPlace(board, row + 2, col - 2) == board.getLastPlayerIndex())
+                score += 1;
         }
 
-        //Checking from the end of the table
-        for(int i = 6; i>=N-1;i--){
-            for(int j = 0;j< 7-N;j++){
-                int count = 0;
-                for(int x = 0;x< N;x++){
-                    if(state[j+x][i-x] == playerNumber){
-                        count++;
-                    }
-                }
-                if(count == N){
-                    connectedNs++;
-                }
-
-            }
+        if(isValidPlace(board, row + 1 , col + 1) == board.getLastPlayerIndex()) {
+            score += 2;
+            if (isValidPlace(board, row + 2, col + 2) == board.getLastPlayerIndex()) {
+                score += 2;
+            }else if(isValidPlace(board, row + 2, col + 2) == 0)
+                score += 1;
+        }else if(isValidPlace(board, row + 1, col + 1) == 0){
+            score += 1;
+            if(isValidPlace(board, row + 2, col + 2) == board.getLastPlayerIndex())
+                score += 1;
         }
 
-        return connectedNs;
-    }
-
-    /**
-     * For checking if there are any possible N-s in a row.
-     * @param N - number of the same numbers in a row
-     * @return the number of possible Ns
-     */
-    private int checkForNsInARow(int N, int[][] state,int lastPlayer,int playerNumber){
-
-        int connectedNs = 0;
-
-        for(int i = 0; i<6;i++){
-            int piecesUnder = 0;
-            int rememberedPiece = 0;
-            for(int j = 0;j<7;j++){
-                if(rememberedPiece == 0 && state[i][j] == playerNumber){
-                    rememberedPiece =  state[i][j];
-                    piecesUnder++;
-                }
-                else if(rememberedPiece == state[i][j] && state[i][j] == playerNumber){
-                    rememberedPiece = state[i][j];
-                    piecesUnder++;
-                    if(piecesUnder == N){
-                        connectedNs++;
-                    }
-                }
-                else if(rememberedPiece != state[i][j]){
-                    rememberedPiece = state[i][j];
-                    piecesUnder = 1;
-                }
-
-            }
+        // IN ROW
+        if(isValidPlace(board, row , col - 1) == board.getLastPlayerIndex()) {
+            score += 2;
+            if(isValidPlace(board, row , col - 2) == board.getLastPlayerIndex()) {
+                score += 2;
+            }else if(isValidPlace(board, row , col - 2) == 0)
+                score += 1;
+        }
+        else if(isValidPlace(board, row, col - 1) == 0){
+            score += 1;
+            if(isValidPlace(board, row, col - 2) == board.getLastPlayerIndex())
+                score += 1;
         }
 
-        return connectedNs;
-    }
-
-    /**
-     * For checking if there are any possible N-s in a column.
-     * @param N - number of the same numbers in a column
-     * @return the number of possible Ns
-     */
-    private int checkForNsInAColumn(int N, int[][] state,int lastPlayer,int playerNumber){
-
-        int connectedNs = 0;
-
-        for(int i = 0; i<7;i++){
-            int piecesUnder = 0;
-            int rememberedPiece = 0;
-            for(int j = 5;j>=0;j--){
-                if(rememberedPiece == 0 && state[j][i] == playerNumber){
-                    rememberedPiece =  state[j][i];
-                    piecesUnder++;
-                }
-                else if(rememberedPiece == state[j][i] && state[j][i] == playerNumber){
-                    rememberedPiece = state[j][i];
-                    piecesUnder++;
-                    if(piecesUnder == N){
-                        connectedNs++;
-                    }
-                }
-                else if(rememberedPiece != state[j][i]){
-                    rememberedPiece = state[j][i];
-                    piecesUnder = 1;
-                }
-
-            }
+        if(isValidPlace(board, row , col + 1) == board.getLastPlayerIndex()) {
+            score += 2;
+            if(isValidPlace(board, row , col + 2) == board.getLastPlayerIndex()) {
+                score += 2;
+            }else if(isValidPlace(board, row , col + 2) == 0)
+                score += 1;
         }
-        return connectedNs;
+        else if(isValidPlace(board, row, col + 1) == 0){
+            score += 1;
+            if(isValidPlace(board, row, col + 2) == board.getLastPlayerIndex())
+                score += 1;
+        }
+
+        // UPPER
+        if(isValidPlace(board, row  - 1 , col) == 0) {
+            score += 1;
+            if(isValidPlace(board, row  - 2 , col) == 0)
+                score += 1;
+        }
+
+        if(isValidPlace(board, row - 1 , col - 1) == board.getLastPlayerIndex()) {
+            score += 2;
+            if(isValidPlace(board, row - 2 , col - 2) == board.getLastPlayerIndex()) {
+                score += 2;
+            }else if(isValidPlace(board, row - 2, col - 2) == 0)
+                score += 1;
+        }else if(isValidPlace(board, row - 1, col - 1) == 0){
+            score += 1;
+            if(isValidPlace(board, row - 2, col - 2) == board.getLastPlayerIndex())
+                score += 1;
+        }
+
+        if(isValidPlace(board, row - 1 , col + 1) == board.getLastPlayerIndex()) {
+            score += 2;
+            if(isValidPlace(board, row - 2 , col + 2) == board.getLastPlayerIndex()) {
+                score += 2;
+            }else if(isValidPlace(board, row - 2, col + 2) == 0)
+                score += 1;
+        }else if(isValidPlace(board, row - 1, col + 1) == 0){
+            score += 1;
+            if(isValidPlace(board, row - 2, col + 2) == board.getLastPlayerIndex())
+                score += 1;
+        }
+
+        return board.getLastPlayerIndex() == 2 ? score : -score;
     }
-
-
 
 }
+
+
